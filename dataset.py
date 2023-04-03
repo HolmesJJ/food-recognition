ea17d7655ca76ec3ca58d9467aa96deb3b493224
HU JIAJUN <hujiajun19961024@gmail.com>
2023-04-03 17:39:14 +0800

    first commit

---
+++ dataset.py
@@ -0,0 +1,43 @@
+import os
+import glob
+import random
+import shutil
+
+RAW_DATA_DIRS = glob.glob("foodsg-5/*")
+DATASET_PATH = "dataset/"
+TRAIN_RATIO = 0.8
+VAL_RATIO = 0.15
+TEST_RATIO = 0.05
+
+
+def create_dataset():
+    os.makedirs(DATASET_PATH)
+
+    for raw_data_dir in RAW_DATA_DIRS:
+        os.makedirs(DATASET_PATH + "train/" + os.path.basename(raw_data_dir))
+        os.makedirs(DATASET_PATH + "val/" + os.path.basename(raw_data_dir))
+        os.makedirs(DATASET_PATH + "test/" + os.path.basename(raw_data_dir))
+
+    for raw_data_dir in RAW_DATA_DIRS:
+        food = glob.glob(f"{raw_data_dir}/*")
+        full_indices = range(len(food))
+        train_indices = random.sample(full_indices, int(len(full_indices) * TRAIN_RATIO))
+        val_test_indices = list(set(full_indices) - set(train_indices))
+        val_indices = random.sample(val_test_indices, int(len(full_indices) * VAL_RATIO))
+        test_indices = list(set(val_test_indices) - set(val_indices))
+        print(raw_data_dir)
+        print(len(full_indices), (len(train_indices) + len(val_indices) + len(test_indices)))
+        for idx in train_indices:
+            shutil.copyfile(food[idx], DATASET_PATH + "train/" + os.path.basename(raw_data_dir) + "/" +
+                            os.path.basename(food[idx]))
+        for idx in val_indices:
+            shutil.copyfile(food[idx], DATASET_PATH + "val/" + os.path.basename(raw_data_dir) + "/" +
+                            os.path.basename(food[idx]))
+        for idx in test_indices:
+            shutil.copyfile(food[idx], DATASET_PATH + "test/" + os.path.basename(raw_data_dir) + "/" +
+                            os.path.basename(food[idx]))
+
+
+if __name__ == '__main__':
+    if not os.path.isdir(DATASET_PATH):
+        create_dataset()
---
+++ train.py
@@ -0,0 +1,124 @@
+# https://www.kaggle.com/code/karan842/pneumonia-detection-transfer-learning-94-acc
+# https://www.kaggle.com/code/elcaiseri/mnist-simple-cnn-keras-accuracy-0-99-top-1#4.-Evaluate-the-model
+# https://keras.io/api/applications/
+
+import os
+import glob
+import pickle
+import matplotlib.pyplot as plt
+
+from keras.models import Model
+from keras.layers import Dense
+from keras.layers import GlobalAveragePooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import EarlyStopping
+from keras.applications import EfficientNetV2M
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+
+DATASET_DIRS = glob.glob("dataset/*")
+TRAIN_PATH = "dataset/train/"
+VAL_PATH = "dataset/val/"
+TEST_PATH = "dataset/test/"
+TRAIN_DIRS = glob.glob("dataset/train/*")
+VAL_DIRS = glob.glob("dataset/val/*")
+TEST_DIRS = glob.glob("dataset/test/*")
+
+BATCH_SIZE = 32
+MODEL_PATH = "model.h5"
+
+
+def show_food(name):
+    food_path = TRAIN_PATH + name
+    food = os.listdir(food_path)
+
+    plt.figure(figsize=(15, 5))
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        img = plt.imread(os.path.join(food_path, food[i]))
+        plt.title(name)
+        plt.imshow(img, cmap="gray")
+        plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
+def run_data_augmentation():
+    img_Datagen = ImageDataGenerator(
+        rescale=1 / 255,
+        shear_range=10,
+        zoom_range=0.3,
+        horizontal_flip=True,
+        vertical_flip=True,
+        brightness_range=[0.5, 2.0],
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rotation_range=20,
+        fill_mode="nearest"
+    )
+    val_test_Datagen = ImageDataGenerator(
+        rescale=1 / 255
+    )
+    train_data = img_Datagen.flow_from_directory(TRAIN_PATH, batch_size=BATCH_SIZE, class_mode="categorical")
+    val_data = val_test_Datagen.flow_from_directory(VAL_PATH, batch_size=BATCH_SIZE, class_mode="categorical")
+    test_data = val_test_Datagen.flow_from_directory(TEST_PATH, batch_size=BATCH_SIZE, class_mode="categorical")
+    return train_data, val_data, test_data
+
+
+def compile_model():
+    net = EfficientNetV2M(
+        weights="imagenet",
+        include_top=False,
+    )
+    for layer in net.layers:
+        layer.trainable = False
+    x = net.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(128, activation="relu")(x)
+    predictions = Dense(len(TRAIN_DIRS), activation="softmax")(x)
+    model = Model(inputs=net.input, outputs=predictions)
+    early_stopping = EarlyStopping(monitor="val_accuracy", mode="max", patience=10, restore_best_weights=True)
+    lr = ReduceLROnPlateau(monitor="val_accuracy", mode="max", patience=10)
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    print(model.summary())
+    return model, early_stopping, lr
+
+
+def train():
+    train_data, val_data, test_data = run_data_augmentation()
+    model, early_stopping, lr = compile_model()
+    history = model.fit(train_data, epochs=100,
+                        validation_data=val_data,
+                        callbacks=[early_stopping, lr],
+                        batch_size=BATCH_SIZE)
+
+    train_score = model.evaluate(train_data)
+    print("Test Loss: ", train_score[0])
+    print("Test Accuracy: ", train_score[1])
+
+    test_score = model.evaluate(test_data)
+    print("Test Loss: ", test_score[0])
+    print("Test Accuracy: ", test_score[1])
+
+    pickle.dump(model, open(MODEL_PATH, "wb"))
+
+    plt.figure(figsize=(12, 8))
+    plt.title("EVALUATION OF VGG19")
+    plt.subplot(2, 2, 1)
+    plt.plot(history.history["loss"], label="Loss")
+    plt.plot(history.history["val_loss"], label="Val_Loss")
+    plt.legend()
+    plt.title("Loss Evolution")
+    plt.subplot(2, 2, 2)
+    plt.plot(history.history["accuracy"], label="Accuracy")
+    plt.plot(history.history["val_accuracy"], label="Val_Accuracy")
+    plt.legend()
+    plt.title("Accuracy Evolution")
+    plt.show()
+
+
+if __name__ == '__main__':
+    # show_food("Apple")
+    train()
